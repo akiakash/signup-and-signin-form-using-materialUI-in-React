211db301a33f85c00f4e3cf44605469216f5249c
akash <akiagash12@gmail.com>
2021-09-26 21:02:55 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SignInSide from "./SignInSide";
+import SignUpSide from "./SignUpSide";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={SignInSide} />
+        <Route exact path="/signup" component={SignUpSide} />
+      </div>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
